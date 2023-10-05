fe68ab2a1c3c40c2e2a8da5ab7f138fffa0bd76c
Rocio <rociocor77@gmail.com>
2023-10-05 16:29:45 +0200

    EJ03

---
+++ EJ03.java
@@ -0,0 +1,14 @@
+package Capitulo2;
+/*Crea las variables nombre, direccion y telefono y asígnales los valores correspondientes.
+Muestra los valores de esas variables por pantalla de tal forma que el
+resultado del programa sea el mismo que en el ejercicio 2.*/
+public class EJ03 {
+    public static void main(String[] args) {
+        String nombre = "Rocío Corbacho Millán";
+        String direccion = "Avenida Dolores Rodríguez de Aragón 6";
+        String telefono = "686272008";
+        System.out.println(nombre);
+        System.out.println(direccion);
+        System.out.println(telefono);
+    }
+}
