4e6e9b9ee0061cb4832ffb1f53ffe2d34078c437
Rocio <rociocor77@gmail.com>
2023-10-05 16:11:45 +0200

    EJ01

---
+++ EJ01.java
@@ -0,0 +1,16 @@
+package Capitulo2;
+/*Escribe un programa en el que se declaren las variables enteras x e y. Asígnales
+los valores 144 y 999 respectivamente. A continuación, muestra por pantalla
+el valor de cada variable, la suma, la resta, la división y la multiplicación. */
+public class EJ01 {
+    public static void main(String[] args) {
+        int x = 144;
+        int y = 999;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("x+y= " + (x + y));
+        System.out.println("x-y= " + (x - y));
+        System.out.println("x*y= " + (x * y));
+        System.out.println("x/y= " + ((double)x/(double)y));
+    }
+}
