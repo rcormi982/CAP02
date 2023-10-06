2cfc90e5f45482bf3268d505a609736c04196145
Rocio <rociocor77@gmail.com>
2023-10-06 13:01:02 +0200

    EJ07

---
+++ EJ07.java
@@ -0,0 +1,14 @@
+package Capitulo2;
+/*Escribe un programa que declare variables de tipo char y de tipo String. Intenta
+mostrarlas por pantalla todas juntas en la misma línea y con una sola sentencia
+de Java (con un solo println) ¿es posible?*/
+public class EJ07 {
+    public static void main(String[] args) {
+            char h = 'h';
+            char o = 'o';
+            char l = 'l';
+            char a = 'a';
+            String palabra = "hola";
+            System.out.println(h + "" + o + l + a + " " + palabra);
+    }
+}
