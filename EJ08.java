ed821aa5f11055f1dd011150a2009e8337194b2d
Rocio <rociocor77@gmail.com>
2023-10-08 16:02:16 +0200

    EJ08

---
+++ EJ08.java
@@ -0,0 +1,15 @@
+package Capitulo2;
+/*Escribe un programa que declare 5 variables de tipo char. A continuación, crea
+otra variable como cadena de caracteres y asígnale como valor la concatenación
+de las anteriores 5 variables. Por último, muestra la cadena de caracteres
+por pantalla ¿Qué problemas te encuentras? ¿cómo lo has solucionado?*/
+public class EJ08 {
+    public static void main(String[] args) {
+        char h = 'h';
+        char o = 'o';
+        char l = 'l';
+        char a = 'a';
+        String palabra ="" + h + o + l + a;
+        System.out.println(palabra);
+    }
+}
